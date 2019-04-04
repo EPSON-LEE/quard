944594610779584c588cd5d3ad634a00f3cd1331
lijiahao <lijiahao@keep.com>
2019-04-04 14:58:34 +0800

    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  // base: 'docs/.vuepress/config.js',
+  title: 'superYipe',
+  description: 'Just playing around',
+  repo: 'https://github.com/txs1992/mt-blog', // 添加 github 链接
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }]
+  ],
+  port:65,
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'Test', link: '/test/' },
+      { text: 'External', link: 'https://google.com' },
+      {
+        text: 'github',
+        // 这里是下拉列表展现形式。
+        items: [
+          { text: 'focus-outside', link: 'https://github.com/txs1992/focus-outside' },
+          { text: 'stylus-converter', link: 'https://github.com/txs1992/stylus-converter' }
+        ]
+      }
+    ],
+    // 为以下路由添加侧边栏
+    sidebar: {
+      '/guide/': [
+        'one',
+        'two'
+      ],
+      '/test/': [
+        'three',
+        'four'
+      ]
+    }
+  },
+  configureWebpack: (config, isServer) => {
+    // webpack配置对象
+    // if (!isServer) {
+    //   // 修改客户端的 webpack 配置
+    // }
+  },
+}
---
+++ docs/guide/two.md
@@ -0,0 +1,1 @@
+# guild two page
